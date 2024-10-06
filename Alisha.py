3abb63f017173e06f9e885bf9c95cbd0ac396014
ALISHA-BALOCH <bhattisahab22770@gmail.com>
2024-10-06 19:18:12 +0500

    Add files via upload

---
+++ Alisha.py
@@ -0,0 +1,14 @@
+import os, platform, time, sys
+print('\033[1;97m [●] Join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/FcQmyk9IVEhIwu3b7rGBfQ')
+os.system('clear')
+print('\033[1;97m [●] Checking For Update...')
+os.system('git pull --quiet 2>/dev/null')
+os.system('clear')
+bit = platform.architecture()[0]
+if bit == '64bit':
+   print('\033[1;97m [✓] Found 64 Bit Device')
+   import Alisha64
+elif bit == '32bit':
+   print('\033[1;91m [✓] Found 32 Bit Device')
+   exit('\033[1;91m [×] Sorry Brother 32 Bit Device Not Supported')
