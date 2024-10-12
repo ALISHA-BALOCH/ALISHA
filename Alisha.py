6d991992ff4e1ef7fd7280059b1d1b4f745c2ae6
ALISHA-BALOCH <bhattisahab22770@gmail.com>
2024-10-12 14:16:40 +0500

    Update Alisha.py

--- Alisha.py
+++ Alisha.py
@@ -1,6 +1,6 @@
 import os, platform, time, sys
 print('\033[1;97m [❥] join Whatsapp Group')
-os.system('xdg-open https://chat.whatsapp.com/FcQmyk9IVEhIwu3b7rGBfQ')
+os.system('xdg-open https://chat.whatsapp.com/GIE74ZSkmv9AVxSxtYZbCb')
 os.system('clear')
 print('\033[1;97m [❥] Checking For Update...')
 os.system('git pull --quiet 2>/dev/null')
